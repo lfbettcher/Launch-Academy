7f7972cf69c3c2dab7658748cf446a0d0bd95ebc
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2021-03-30 14:02:24 -0700

    java-shout-detector consecutive exclamations

---
+++ java-shout-detector/ShoutDetector.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ShoutDetector {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Say something");
+
+    do {
+      String userInput = sc.nextLine();
+      System.out.printf("You said: %s\n", userInput);
+
+      if (userInput.contains("!?!?")) {
+        System.out.println("I don't respond to inflammatory questions");
+      } else if (userInput.equals(userInput.toUpperCase()) || userInput.contains("!!")) {
+        System.out.println("Please stop yelling at me.");
+      } else {
+        System.out.println("Ok, thanks.");
+        break;
+      }
+      System.out.println("Please try to share again.");
+    } while (true);
+    sc.close();
+  }
+}
