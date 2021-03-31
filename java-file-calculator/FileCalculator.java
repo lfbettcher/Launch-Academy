9506dd1a4970c701fa164e0564418a3107de539f
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2021-03-31 15:19:12 -0700

    refactor java-file-calculator, print whole equation

--- java-file-calculator/FileCalculator.java
+++ java-file-calculator/FileCalculator.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.StringJoiner;
 
 public class FileCalculator {
 
@@ -28,56 +29,67 @@ public class FileCalculator {
     }
 
     Scanner scFile = new Scanner(problemsFile);
-
     while (scFile.hasNextLine()) {
       String c = scFile.nextLine();
       if (OPERATIONS.contains(c)) {
-        ArrayList<Integer> nums = new ArrayList<>();
-        while (scFile.hasNextInt()) {
-          nums.add(scFile.nextInt());
-        }
-        if (nums.isEmpty()) {
-          writeFile("No numbers to " + c);
-        } else {
-          double result = arithmetic(c, nums);
-          if (result % 1 == 0) {
-            writeFile(Integer.toString((int) result));
-          } else if (Double.isInfinite(result)) {
-            writeFile("undefined");
-          } else {
-            String rounded = new BigDecimal(result).setScale(2, RoundingMode.HALF_UP).toString();
-            writeFile("\u2248 " + rounded);
-          }
-        }
+        ArrayList<Integer> nums = getNumbers(scFile);
+        String end = scFile.nextLine();
+        writeFile("=".equals(end) ? calculate(c, nums) : "Invalid end to problem: " + end);
       }
     }
     scFile.close();
     System.out.println("Your math homework is done!");
   }
 
-  private static double arithmetic(String operation, ArrayList<Integer> numbers) {
-    if (numbers.isEmpty()) {
-      return 0;
+  private static ArrayList<Integer> getNumbers(Scanner sc) {
+    ArrayList<Integer> nums = new ArrayList<>();
+    while (sc.hasNextInt()) {
+      nums.add(sc.nextInt());
     }
+    sc.nextLine(); // consumes "\n" from last int read
+    return nums;
+  }
+
+  private static String calculate(String operation, ArrayList<Integer> numbers) {
+    if (numbers.isEmpty()) {
+      return "No numbers to " + operation;
+    }
+    StringJoiner equationSJ = new StringJoiner(" " + operation + " ", "", " = ");
+    equationSJ.add(String.valueOf(numbers.get(0)));
+
     double result = numbers.get(0);
     for (int i = 1; i < numbers.size(); ++i) {
       int num = numbers.get(i);
-      switch (operation) {
-        case "*":
-          result *= num;
-          break;
-        case "+":
-          result += num;
-          break;
-        case "-":
-          result -= num;
-          break;
-        case "/":
-          result /= num;  // no Exception when double / 0
-          break;
-      }
+      result = arithmetic(result, num, operation);
+      equationSJ.add(String.valueOf(num));
     }
-    return result;
+    String equation = equationSJ.toString();
+    String resultString;
+    if (result % 1 == 0) {
+      resultString = String.valueOf((int) result);
+    } else if (Double.isInfinite(result)) {
+      resultString = "undefined";
+    } else {
+      equation = equation.substring(0, equation.length() - 2);
+      String rounded = new BigDecimal(result).setScale(2, RoundingMode.HALF_UP).toString();
+      resultString = "\u2248 " + rounded;
+    }
+    return equation + resultString;
+  }
+
+  private static double arithmetic(double a, double b, String operation) {
+    switch (operation) {
+      case "*":
+        return a * b;
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+      case "/":
+        return a / b;
+      default:
+        return 0;
+    }
   }
 
   private static void writeFile(String text) throws IOException {
