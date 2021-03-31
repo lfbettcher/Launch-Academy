e02b78c49fdf4a523104776024c84acb46df4bd6
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2021-03-31 09:48:44 -0700

    java-space-bulletin and looping

--- java-looping-space-bulletin/Spaceship.java
+++ java-looping-space-bulletin/Spaceship.java
@@ -1,9 +1,52 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Spaceship {
+
+  private static final String CAPTAIN_FILE = "captainMsg.txt";
   private static final String INPUT_FILE = "crewManifest.txt";
 
   public static void main(String[] args) throws IOException {
+    // Reading the Manifest
+    Scanner scFile = new Scanner(new File(INPUT_FILE));
+    while (scFile.hasNextLine()) {
+      String[] crewMember = scFile.nextLine().split(" ", 2);
+      if ("Captain".equals(crewMember[0])) {
+        System.out.printf("*%s %s\n", crewMember[0], crewMember[1]);
+      } else {
+        System.out.printf("%s - %s\n", crewMember[0], crewMember[1]);
+      }
+    }
+    scFile.close();
 
+    // Display Captain's Messages
+    File captainFile = new File(CAPTAIN_FILE);
+    if (captainFile.exists()) {
+      System.out.println("\nThe Captain's message:\n" + getFileContent(captainFile).trim());
+    }
+
+    // Writing to the Messages File
+    Scanner sc = new Scanner(System.in);
+    System.out.println("\nCaptain, leave a message:");
+    String line = sc.nextLine();
+    StringBuilder captainMsg = new StringBuilder();
+    while (!"done".equalsIgnoreCase(line)) {
+      captainMsg.append(line).append("\n");
+      System.out.println("Leave another message or 'done' to quit.");
+      line = sc.nextLine();
+    }
+    sc.close();
+
+    FileWriter fw = new FileWriter("./captainMsg.txt", true);
+    if (!captainMsg.toString().isBlank()) {
+      fw.write(captainMsg.toString() + "\n");
+    }
+    fw.close();
+  }
+
+  private static String getFileContent(File file) throws IOException {
+    return new Scanner(file).useDelimiter("\\z").next();
   }
 }
--- java-space-bulletin/Spaceship.java
+++ java-space-bulletin/Spaceship.java
@@ -1,16 +1,47 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Spaceship {
+
   public static final String INPUT_FILE = "crewManifest.txt";
+  public static final String CAPTAIN_FILE = "captainMsg.txt";
 
   /**
-   * Print the crew and docking or leaving time for the ship.
+   * Print the crew manifest and save the Captain's message. When the program runs a second time,
+   * print Captain's message.
    *
-   * @param args either 'd' for departure or "l" for leaving.
    * @throws IOException
    */
   public static void main(String[] args) throws IOException {
+    File captainFile = new File(CAPTAIN_FILE);
+
+    if (captainFile.exists()) {
+      System.out.println("The Captain's message:\n" + getFileContent(captainFile));
+      return;
+    }
+
+    System.out.println(getFileContent(new File(INPUT_FILE)));
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Captain, please leave a message:");
+    StringBuilder captainMsg = new StringBuilder(sc.nextLine());
+    while (sc.hasNextLine()) {
+      String line = sc.nextLine();
+      if (line == null || line.isEmpty()) {
+        break;
+      }
+      captainMsg.append("\n").append(line);
+    }
+    sc.close();
+
+    FileWriter fw = new FileWriter("./captainMsg.txt");
+    fw.write(captainMsg.toString());
+    fw.close();
+  }
+
+  private static String getFileContent(File file) throws IOException {
+    return new Scanner(file).useDelimiter("\\z").next();
   }
 }
